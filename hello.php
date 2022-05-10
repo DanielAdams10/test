64cb5abbec8a73c6345234daba00befc13a54697
K4Kiyoshi <96560380+K4Kiyoshi@users.noreply.github.com>
2022-05-10 15:41:23 +0630

    change tl kwr

--- hello.php
+++ hello.php
@@ -1,20 +1,12 @@
-<?php 
-
-echo "Hello Baby";
-
-echo "Have a nice day";
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
 <body>
-    <h1>Hello World</h1>
-    <h2>This is push test</h2>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+
 </body>
 </html>
-?>

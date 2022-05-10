ee48722f2fb414226e431a9733131996b5a48947
DanielAD10 <adamsdaniel171@gmail.com>
2022-05-10 15:06:35 +0630

    update hello.php

--- hello.php
+++ hello.php
@@ -3,4 +3,17 @@
 echo "Hello Baby";
 
 echo "Have a nice day";
-ksfhiuh hfuihifauh fiuewhnkniuh
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
+?>
